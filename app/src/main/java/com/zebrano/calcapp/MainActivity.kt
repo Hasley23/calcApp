4928022707194cd7032fb9af4683ec7cad0e1707
Hasley23 <hasleyclone@gmail.com>
2022-11-08 17:50:58 +0300

    conditions researh app added

--- app/src/main/java/com/zebrano/calcapp/MainActivity.kt
+++ app/src/main/java/com/zebrano/calcapp/MainActivity.kt
@@ -6,8 +6,8 @@ import com.zebrano.calcapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
-    val first = 88
-    val second = 97
+    val maxPerson = 90
+    var currentPerson = 45
     var res = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,20 +16,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
         bindingClass.button.setOnClickListener{
-            res = first + second
-            viewRes()
+            bindingClass.tvRes.text = if (maxPerson > currentPerson)
+                "Всё в порядке!" else "Превышение!"
+
         }
+        var abda = 0
         bindingClass.button2.setOnClickListener{
-            res = first - second
-            viewRes()
+            when(currentPerson){
+                //45 -> bindingClass.tvRes.text = "Ровно половина!"
+                //89 -> bindingClass.tvRes.text = "Почти полный зал!"
+                in 0..maxPerson -> bindingClass.tvRes.text = "Всё в порядке!"
+                else -> abda = 5
+            }
         }
         bindingClass.button3.setOnClickListener{
-            res = first * second
-            viewRes()
+            bindingClass.tvRes.text = "Это ни к чему не привело"
         }
     }
     fun viewRes(){
-        val str = "Результат математического действия: $res"
+        val str = ": $res"
         if (res != 0)
             bindingClass.tvRes.text = str
     }
